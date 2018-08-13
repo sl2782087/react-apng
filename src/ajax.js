e4fc290ff3394a6177ce5dbccf382823fba6502b
neal <neal.wang@pdabc.com>
2018-08-13 16:40:31 +0800

    ajax请求优化

--- src/ajax.js
+++ src/ajax.js
@@ -1,16 +1,21 @@
 function getImgBuffer(url) {
-  return new Promise(((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', url);
     xhr.responseType = 'arraybuffer';
-    xhr.onload = function () {
-      if (this.status === 200) {
-        resolve(this.response);
-      } else {
-        reject(this);
-      }
-    };
+    if (xhr.readyState === 4 && (xhr.status === 200 || xhr.status === 304)) {
+      resolve(this.response);
+    } else {
+      reject(new Error(this));
+    }
+    // xhr.onload = function () {
+    //   if (this.status === 200) {
+    //     resolve(this.response);
+    //   } else {
+    //     reject(this);
+    //   }
+    // };
     xhr.send();
-  }));
+  });
 }
 export { getImgBuffer };
